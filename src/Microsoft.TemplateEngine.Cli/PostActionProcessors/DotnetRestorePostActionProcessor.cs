9d28e64c00a36913385d13ef0b316db1d7f0b933
Sean Peters <sepeters@microsoft.com>
2017-10-31 15:13:58 -0700

    Allow .sln files to be processed by restore

--- src/Microsoft.TemplateEngine.Cli/PostActionProcessors/DotnetRestorePostActionProcessor.cs
+++ src/Microsoft.TemplateEngine.Cli/PostActionProcessors/DotnetRestorePostActionProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using Microsoft.TemplateEngine.Abstractions;
 
@@ -28,8 +28,12 @@ namespace Microsoft.TemplateEngine.Cli.PostActionProcessors
             {
                 string pathToRestore = !string.IsNullOrEmpty(outputBasePath) ? Path.Combine(outputBasePath, output.Path) : output.Path;
 
-                if (string.IsNullOrEmpty(pathToRestore) || (!Directory.Exists(pathToRestore) && !Path.GetExtension(pathToRestore).EndsWith("proj", StringComparison.OrdinalIgnoreCase)))
-                {
+                if (string.IsNullOrEmpty(pathToRestore) ||
+                    (!Directory.Exists(pathToRestore)
+                        && !Path.GetExtension(pathToRestore).EndsWith("proj", StringComparison.OrdinalIgnoreCase)
+                        && !Path.GetExtension(pathToRestore).Equals(".sln", StringComparison.OrdinalIgnoreCase)
+                    ))
+                {       
                     continue;
                 }
 
